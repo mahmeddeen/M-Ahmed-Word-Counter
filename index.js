59450f7a729c91ec2a4661f3abb35075588bbc2a
M Ahmed Deen <75785048+mahmeddeen@users.noreply.github.com>
2024-04-30 10:28:53 +0500

    Add files via upload
    This project is used for word counter.

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Display a colourful message 
+console.log(chalk.bold.cyanB right("\n \t\t Welcome M Ahmed Gujjar"));
+console.log("-".repeat(50));
+// prompt the user to enter a sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence",
+    }
+]);
+//  Triming the sentence spiliting it into words based on -space-
+let words = answers.sentence.trim().split(" ");
+// Analysis of user input sentence 
+console.log("=".repeat(50));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("-".repeat(50));
